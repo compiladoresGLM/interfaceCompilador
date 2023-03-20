ca7beb17bd9c455cd803b4a01ade9e997bb292e1
Guilherme Milani <gui26milani@gmail.com>
2023-03-20 14:25:44 -0300

    feat: Adicionado método abrir arquivo

--- src/main/java/interfacecompilador/interfacecompilador/InterfaceController.java
+++ src/main/java/interfacecompilador/interfacecompilador/InterfaceController.java
@@ -3,12 +3,15 @@ package interfacecompilador.interfacecompilador;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.stream.IntStream;
 
 public class InterfaceController {
     @FXML
@@ -22,6 +25,9 @@ public class InterfaceController {
 
     @FXML
     private Label labelStatus;
+
+    @FXML
+    private TextArea linhas;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -70,7 +76,60 @@ public class InterfaceController {
             areaMensagem.setText("");
         }
     }
+    private void adicionarLinhas() {
+        int linhasExistentes = getQuantidadeLinhas();
+        int tamUltimaLinhaAnterior = getTamanhoUltimaLinha();
+        IntStream.range(0, areaCodigo.getParagraphs().size()).forEach(x -> {
+            int linhaAtual = x + 1;
+            if (linhaAtual > linhasExistentes) {
+                linhas.setText(linhas.getText().concat("\n" + linhaAtual));
+            }
+        });
+        redefinirLarguraLista(tamUltimaLinhaAnterior);
+    }
 
+    private void removerLinhas() {
+        int linhasExistentes = getQuantidadeLinhas();
+        int tamUltimaLinhaAnterior = getTamanhoUltimaLinha();
+        IntStream.range(1, linhasExistentes).forEach(x -> {
+            int linhaAtual = x + 1;
+            if (linhaAtual > areaCodigo.getParagraphs().size()) {
+                linhas.setText(linhas.getText().replaceFirst("\n" + linhaAtual, ""));
+            }
+        });
+        redefinirLarguraLista(tamUltimaLinhaAnterior);
+    }
+
+    private int getTamanhoUltimaLinha() {
+        String[] todasLinhas = linhas.getText().split("\n");
+        return todasLinhas[todasLinhas.length - 1].length();
+    }
+    private int getQuantidadeLinhas() {
+        return linhas.getText().split("\n").length;
+    }
+
+
+    private void redefinirLarguraLista(int tamUltimaLinhaAnterior) {
+        int tamUltimaLinha = getTamanhoUltimaLinha();
+        if (tamUltimaLinhaAnterior != tamUltimaLinha) {
+            double newScale = (tamUltimaLinha == 2 ? 0.6 : (tamUltimaLinha == 1 ? 1 : 0.5));
+            linhas.setMinWidth(20 * tamUltimaLinha * newScale);
+            linhas.setMaxWidth(20 * tamUltimaLinha * newScale);
+        }
+    }
+
+    public void alterarContadorDeLinhaAoDigitar(KeyEvent keyEvent) {
+        if (keyEvent.getCode().equals(KeyCode.ENTER)
+                || (keyEvent.getCode().equals(KeyCode.V) && keyEvent.isControlDown())) {
+            adicionarLinhas();
+        }
+        if (keyEvent.getCode().equals(KeyCode.BACK_SPACE) || keyEvent.getCode().equals(KeyCode.DELETE)
+                || (keyEvent.getCode().equals(KeyCode.X) && keyEvent.isControlDown())) {
+            if (areaCodigo.getParagraphs().size() != getQuantidadeLinhas()) {
+                removerLinhas();
+            }
+        }
+    }
 
 
 
