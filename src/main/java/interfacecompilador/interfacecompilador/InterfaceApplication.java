f9b9ee7a8e7403ad5e6022e10c196b1dc22aa8b4
Mateus Maas <maas.mateus@gmail.com>
2023-03-20 09:54:25 -0300

    Ajuste nas imagens do botão e tamanho da tela

--- src/main/java/interfacecompilador/interfacecompilador/InterfaceApplication.java
+++ src/main/java/interfacecompilador/interfacecompilador/InterfaceApplication.java
@@ -15,6 +15,8 @@ public class InterfaceApplication extends Application {
         stage.setTitle("Compilador");
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
