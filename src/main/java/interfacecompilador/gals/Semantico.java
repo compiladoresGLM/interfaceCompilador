1fda9129b480270a8c1b47e2f7c7966d4635c58a
guilhermeMilani <gui26milani@gmail.com>
2023-06-05 20:34:02 -0300

    criação do acionaToken1()

--- src/main/java/interfacecompilador/gals/Semantico.java
+++ src/main/java/interfacecompilador/gals/Semantico.java
@@ -1,9 +1,28 @@
 package interfacecompilador.gals;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 public class Semantico implements Constants
 {
+    private static final Stack<String> pilha = new Stack<>();
+
+    private void validaTiposAritiméticos(String tipo1, String tipo2)throws SemanticError{
+        List<String> tiposValidos = Arrays.asList("float64","int64");
+        if (!tiposValidos.contains(tipo1)||!tiposValidos.contains(tipo2)){
+            throw new SemanticError("tipo(s) incompatível(is) na expressão aritmética");
+        }
+    }
     public void executeAction(int action, Token token)	throws SemanticError
     {
         System.out.println("Ação #"+action+", Token: "+token);
-    }	
+    }
+    private void acionaToken1() throws SemanticError{
+        String tipo1 = pilha.pop();
+        String tipo2 = pilha.pop();
+
+
+    }
+
 }
