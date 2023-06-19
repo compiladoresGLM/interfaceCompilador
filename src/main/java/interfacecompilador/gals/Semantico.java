07d9faee52c4f2d1db32959be0d2fe268ab7cfc8
Mateus Maas <maas.mateus@gmail.com>
2023-06-18 21:48:01 -0300

    Ações revisadas parcialmente

--- src/main/java/interfacecompilador/gals/Semantico.java
+++ src/main/java/interfacecompilador/gals/Semantico.java
@@ -11,12 +11,21 @@ public class Semantico implements Constants {
     private Map<String, String> tabelaSimbolos = new HashMap<>();
     private int indexRotulo = 0;
     private int numeroRotulo = 0;
-    private String tipoVar;
     private List<String> listaId = new ArrayList<>();
     private String tipoVariavel;
     private static StringJoiner sJoiner = new StringJoiner("\n");
     private String operador;
     private String pathToCompile;
+
+    private final String FLOAT_64 = "float64";
+    private final String INT_64 = "int64";
+    private final String STRING = "string";
+    private final String BOOL = "bool";
+
+    private final String TEMP_INT = "_temp_int";
+    private final String TEMP_FLOAT = "_temp_float";
+    private final String TEMP_STRING = "_temp_string";
+    private final String TEMP_BOOL = "_temp_bool";
 
     public void setPathToCompile(String path) {
         pathToCompile = path;
@@ -82,48 +91,51 @@ public class Semantico implements Constants {
                 acao20(token);
                 break;
             case 21:
-                acao21(token);
-                break;
-            case 24:
-                acao24();
-                break;
-            case 27:
-                acao27();
-                break;
-            case 30:
-                acao30(token);
-                break;
-            case 33:
-                acao33(token);
+                acao21();
                 break;
             case 22:
                 acao22(token);
                 break;
+            case 23:
+                acao23(token);
+                break;
+            case 24:
+                acao24();
+                break;
             case 25:
                 acao25();
+                break;
+            case 27:
+                acao27();
                 break;
             case 28:
                 acao28();
                 break;
+            case 30:
+                acao30(token);
+                break;
             case 31:
                 acao31();
+                break;
+            case 33:
+                acao33(token);
                 break;
         }
 
     }
 
     private void validaTiposAritimeticos(String tipo1, String tipo2) throws SemanticError {
-        List<String> tiposValidos = Arrays.asList("float64", "int64");
+        List<String> tiposValidos = Arrays.asList(FLOAT_64, INT_64);
         if (!tiposValidos.contains(tipo1) || !tiposValidos.contains(tipo2)) {
             throw new SemanticError("tipo(s) incompatível(is) na expressão aritmética");
         }
     }
 
     private void addAritimeticos(String tipo1, String tipo2) {
-        if (tipo1.equals("int64") || tipo2.equals("int64")) {
-            pilha.push("int64");
+        if (tipo1.equals(INT_64) || tipo2.equals(INT_64)) {
+            pilha.push(INT_64);
         } else {
-            pilha.push("float64");
+            pilha.push(FLOAT_64);
         }
     }
 
@@ -150,6 +162,61 @@ public class Semantico implements Constants {
             return lexeme.concat("_compiled");
         }
         return lexeme;
+    }
+
+    private boolean ehTipoCompativel(String tipoVar, String tipoValor) {
+        if (FLOAT_64.equals(tipoVar)) {
+            return Arrays.asList(FLOAT_64, INT_64).contains(tipoValor);
+        } else if (INT_64.equals(tipoVar)) {
+            return INT_64.equals(tipoValor);
+        } else if (STRING.equals(tipoVar)) {
+            return STRING.equals(tipoValor);
+        } else if (BOOL.equals(tipoVar)) {
+            return BOOL.equals(tipoValor);
+        }
+
+        return false;
+    }
+
+    private void atribuicao(String tipoExpressao, String varTemp) throws SemanticError {
+
+        sJoiner.add("ldloc " + varTemp);
+
+        for (int i = 1; i < listaId.size() - 1; i++) {
+            sJoiner.add("dup");
+        }
+
+        for (String id : listaId) {
+
+            if (tabelaSimbolos.get(id).isEmpty()) {
+                sJoiner.add(".locals (%s %s)".formatted(tipoExpressao, id));
+                tabelaSimbolos.put(id, tipoExpressao);
+            } else {
+                if (!ehTipoCompativel(tipoExpressao, tabelaSimbolos.get(id))) {
+                    throw new SemanticError("Tipos incompatíveis em comando de atribuição");
+                }
+            }
+        }
+    }
+
+    private String getClasse(String tipoVariavel) {
+
+        switch (tipoVariavel) {
+            case INT_64 -> {
+                return "Int64";
+            }
+            case FLOAT_64 -> {
+                return "Double";
+            }
+            case STRING -> {
+                return "String";
+            }
+            case BOOL -> {
+                return "Boolean";
+            }
+        }
+
+        throw new RuntimeException("O tipo de variável %s não possui uma classe definida".formatted(tipoVariavel));
     }
 
     private void acao1() throws SemanticError {
@@ -185,21 +252,21 @@ public class Semantico implements Constants {
     }
 
     private void acao5(Token token) {
-        pilha.push("int64");
+        pilha.push(INT_64);
         sJoiner.add("ldc.i8 ".concat(converterConstantesNumericas(token.getLexeme())));
         sJoiner.add("conv.r8");
     }
 
 
     private void acao6(Token token) {
-        pilha.push("float64");
+        pilha.push(FLOAT_64);
         sJoiner.add("ldc.r8 ".concat(converterConstantesNumericas(token.getLexeme())));
     }
 
     private void acao8() {
         String tipo = pilha.pop();
 
-        if ("int64".equals(tipo) || "float64".equals(tipo)) {
+        if (INT_64.equals(tipo) || FLOAT_64.equals(tipo)) {
             pilha.add(tipo);
         } else {
             //todo throw exceprion?
@@ -216,6 +283,9 @@ public class Semantico implements Constants {
 
     private void acao10(Token token) {
 
+        pilha.pop();
+        pilha.pop();
+        
         pilha.add("bool");
 
         switch (token.getLexeme()) {
@@ -260,11 +330,11 @@ public class Semantico implements Constants {
 
     private void acao14() {
         String tipo = pilha.pop();
-        if (tipo.equals("int64")) {
+        if (tipo.equals(INT_64)) {
             sJoiner.add("conv.i8");
         }
         if (tipo.equals("char")) {
-            sJoiner.add("string");
+            sJoiner.add(STRING);
         }
         sJoiner.add("call void [mscorlib]System.Console::Write(" + tipo + ")");
     }
@@ -316,57 +386,104 @@ public class Semantico implements Constants {
     }
 
     private void acao20(Token token) {
-        pilha.push("string");
+        pilha.push(STRING);
         sJoiner.add("ldstr".concat(token.getLexeme()));
     }
 
-    private void acao21(Token token) {
-        pilha.push("string");
-        sJoiner.add("ldstr " + converteTokenString(token));
-    }
-    private void acao24() {
-        indexRotulo++;
-        sJoiner.add("brfalse nr_" + indexRotulo);
-        pilhaRotulo.push("nr_" + indexRotulo);
-    }
-
-    private void acao27() {
-        indexRotulo++;
-        sJoiner.add("nr_" + indexRotulo + ":");
-        pilhaRotulo.push("nr_" + indexRotulo);
-    }
-
-    private void acao30(Token token){
-        if((token.getLexeme()).equals("int")) {
-            tipoVariavel = "int64";
-        }
-        if((token.getLexeme()).equals("float")) {
-            tipoVariavel = "float64";
-        }
-    }
-
-    private void acao33(Token token){
-        String id = converterId(token.getLexeme());
-        String tipoId = tabelaSimbolos.get(id);
-        pilha.push(tipoId);
-        sJoiner.add("ldloc " + id);
-        if (tipoId.equals("int64")){
-            sJoiner.add("conv.r8");
-        }
+    private void acao21() {
+        sJoiner.add(".locals(int64 _temp_int, float64 _temp_float, string _temp_str, bool _temp_bool)");
     }
 
     private void acao22(Token token) {
         listaId.add(converterId(token.getLexeme()));
     }
 
-    private void acao25(){
-        String id = listaId.remove(0);
-        String tipoId = tabelaSimbolos.get(id);
-        pilha.pop();
-        if(tipoId.equals("int64")){
-            sJoiner.add("conv.i8");
+    private void acao23(Token token) throws SemanticError {
+
+        if (listaId.contains(token.getLexeme())) {
+            sJoiner.add("ldloc " + token.getLexeme());
+
+            if (INT_64.equals(tabelaSimbolos.get(token.getLexeme()))) {
+                sJoiner.add("conv.r8");
+            }
+
+            pilha.push(tabelaSimbolos.get(token.getLexeme()));
+
+        } else {
+            throw new SemanticError("identificador " + token.getLexeme() + " não declarado");
         }
-        sJoiner.add("stloc " + id);
+    }
+
+    private void acao24() throws SemanticError {
+
+        String tipoExpressao = pilha.pop();
+        String varTemp = listaId.get(listaId.size() - 1);
+
+
+    }
+
+    private void acao25(){
+
+        String tipoExpressao = pilha.pop();
+
+        switch (tipoExpressao) {
+            case INT_64 -> {
+                sJoiner.add("conv.i8");
+                sJoiner.add("stloc _temp_int");
+                listaId.add(TEMP_INT);
+            }
+            case FLOAT_64 -> {
+                sJoiner.add("stloc _temp_float");
+                listaId.add(TEMP_FLOAT);
+            }
+            case STRING -> {
+                sJoiner.add("stloc _temp_str");
+                listaId.add(TEMP_STRING);
+            }
+            case BOOL -> {
+                sJoiner.add("stloc _temp_bool");
+                listaId.add(TEMP_BOOL);
+            }
+        }
+
+    }
+
+    // todo Verificar com a Prof
+    private void acao26() throws SemanticError {
+
+        String tipoExpressao = pilha.peek();
+        sJoiner.add("brfalse novo_rotulo");
+
+
+
+        String varTemp = listaId.get(listaId.size() - 1);
+        listaId.remove(listaId.size() - 1);
+
+        atribuicao(tipoExpressao, varTemp);
+
+
+
+    }
+
+    private void acao27() throws SemanticError {
+
+        for (String id : listaId) {
+
+            String tipoVar = tabelaSimbolos.get(id);
+
+            // Verifica se a variável já foi declarada
+            if (tipoVar.isEmpty()) {
+                throw new SemanticError("Identificador %s não declarado".formatted(id));
+            }
+
+            sJoiner.add("call string [mscorlib]System.Console::ReadLine()");
+
+            if (!tipoVar.equals(STRING)) sJoiner
+                    .add("call " + tipoVar + " [mscorlib]System." + getClasse(tipoVar) + "::Parse(string)");
+
+        }
+
+
     }
 
     private void acao28(){
@@ -375,9 +492,40 @@ public class Semantico implements Constants {
         pilhaRotulo.push("nr_" + numeroRotulo);
     }
 
+    // todo Maas
+    private void acao29() {
+        indexRotulo++;
+        sJoiner.add("nr_" + numeroRotulo + ":");
+
+        pilhaRotulo.pop();
+    }
+
+    private void acao30(Token token){
+        if((token.getLexeme()).equals("int")) {
+            tipoVariavel = INT_64;
+        }
+        if((token.getLexeme()).equals("float")) {
+            tipoVariavel = FLOAT_64;
+        }
+    }
+
     private void acao31() {
         numeroRotulo++;
         sJoiner.add("nr_" + numeroRotulo + ":");
         pilhaRotulo.push("nr_" + numeroRotulo);
     }
+
+    // todo Maas
+    private void acao32() {}
+
+    private void acao33(Token token){
+        String id = converterId(token.getLexeme());
+        String tipoId = tabelaSimbolos.get(id);
+        pilha.push(tipoId);
+        sJoiner.add("ldloc " + id);
+        if (tipoId.equals(INT_64)){
+            sJoiner.add("conv.r8");
+        }
+    }
+
 }
